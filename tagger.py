2dd361df659651a7dfde5431c75f6668972ed211
Justice Suh <justice.suh@gmail.com>
2017-11-27 20:43:28 -0500

    perceptron tagger

--- tagger.py
+++ tagger.py
@@ -1,6 +1,5 @@
-from nltk import word_tokenize
-from nltk.corpus import treebank
-from nltk.tag import hmm
+from nltk import word_tokenize, pos_tag
+from nltk.tag import hmm, perceptron
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction import DictVectorizer
@@ -121,12 +120,40 @@ class DTCTagger():
         return features, tags
 
 
+class PerceptronTagger():
+    def __init__(self):
+        self.tagger = perceptron.PerceptronTagger(load=False)
+
+    def train(self, filename):
+        self.tagger.train(read_tagged_corpus(filename))
+
+    def tag(self, sentence):
+        return self.tagger.tag(sentence)
+
+    def test(self, filename):
+        test_data = read_tagged_corpus(filename)
+        correct_tags = 0
+        total_tags = 0
+        correct_sents = 0
+        total_sents = len(test_data)
+        for sentence in test_data:
+            pred_tagging = self.tag(list(map(lambda x: x[0], sentence)))
+            for (_, gold_tag), (_, pred_tag) in zip(sentence, pred_tagging):
+                if gold_tag == pred_tag:
+                    correct_tags += 1
+            if sentence == pred_tagging:
+                correct_sents += 1
+            total_tags += len(sentence)
+        print('Token Accuracy: {} / {} - {:.2f}%'.format(correct_tags, total_tags, correct_tags / total_tags * 100))
+        print('Sentence Accuracy: {} / {} - {:.2f}%'.format(correct_sents, total_sents, correct_sents / total_sents * 100))
+
+
 if __name__ == '__main__':
     TRAIN_DATA = 'data/en-ud-train.upos.tsv'
     TEST_DATA = 'data/en-ud-test.upos.tsv'
     DEV_DATA = 'data/en-ud-dev.upos.tsv'
 
-    tagger = DTCTagger()
+    tagger = PerceptronTagger()
     print('Training Model')
     tagger.train(TRAIN_DATA)
     print('Training Complete')
