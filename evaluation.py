995a4dd1457a0426dc75ec2361d4c65ea8d2848f
Roisin McLoughlin <roisin@thecorp.org>
2017-12-03 20:47:05 -0500

    Added evaluation script from a4

---
+++ evaluation.py
@@ -0,0 +1,62 @@
+from collections import Counter
+
+class Eval:
+    def __init__(self, gold, pred):
+        assert len(gold)==len(pred)
+        self.gold = gold
+        self.pred = pred
+
+    def accuracy(self):
+        numer = sum(1 for p,g in zip(self.pred,self.gold) if p==g)
+        return numer / len(self.gold)
+
+    def precision(self, label):
+    	# True positives / (true positives + false positivies)
+    	true_positives = 0
+    	for i in range(0, len(self.gold)):
+    		if (self.gold[i] == label) and (self.pred[i] == label):
+    			true_positives += 1
+    	false_positives = 0
+    	for i in range(0, len(self.gold)):
+    		if (self.gold[i] != label) and (self.pred[i] == label):
+    			false_positives += 1
+
+    	return true_positives / (true_positives + false_positives)
+
+    def recall(self, label):
+    	# True positives / (true positives + false negatives)
+    	true_positives = 0
+    	for i in range(0, len(self.gold)):
+    		if (self.gold[i] == label) and (self.pred[i] == label):
+    			true_positives += 1
+    	false_negatives = 0
+    	for i in range(0, len(self.gold)):
+    		if (self.gold[i] == label) and (self.pred[i] != label):
+    			false_negatives += 1
+
+    	return true_positives / (true_positives + false_negatives)
+
+    def f1(self, label):
+    	precision = self.precision(label)
+    	recall = self.recall(label)
+    	return 2 * precision * recall / (precision + recall)
+
+    def confusion_matrix(self):
+    	label_indicies = {}
+    	labels = ['ARA', 'DEU', 'FRA', 'HIN', 'ITA', 'JPN', 'KOR', 'SPA', 'TEL', 'TUR', 'ZHO']
+    	ind = 0
+    	for label in labels:
+    		label_indicies[label] = ind
+    		ind += 1
+
+    	matrix = [[0 for x in range(len(labels))] for y in range(len(labels))] 
+    	for i in range(len(self.gold)):
+    		row = label_indicies[self.pred[i]]
+    		col = label_indicies[self.gold[i]]
+    		matrix[row][col] += 1
+
+    	return matrix
+
+
+
+
